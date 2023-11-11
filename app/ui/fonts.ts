ed337c95fbf36802f621666cb3f4ad9408a6fb86
Shakhverdian, Andrii <shakhverdiana@ryanair.com>
2023-11-11 16:08:06 +0000

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inder, Lusitana } from "next/font/google";
+
+export const inter = Inder({ weight: "400", subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
